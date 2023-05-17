915988b9980e8573dc77315d79248b656c538dba
Rafael de Luca <deluca1712@gmail.com>
2023-05-17 16:27:40 -0300

    Criando backend em java spring

---
+++ backend/src/main/java/com/rafaeldeluca/provaconceitofirebasecloudmessage/ProvaConceitoFirebaseCloudMessageApplication.java
@@ -0,0 +1,13 @@
+package com.rafaeldeluca.provaconceitofirebasecloudmessage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProvaConceitoFirebaseCloudMessageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProvaConceitoFirebaseCloudMessageApplication.class, args);
+	}
+
+}
